d6e459213f6f96965496a4bf2dc55eb2b8a520b4
dangrizky <rizkylossaga3@gmail.com>
2025-08-11 16:31:00 +0700

    ahai

--- public/sw.js
+++ public/sw.js
@@ -1,36 +1,61 @@
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open("koze-cache-v1").then((cache) => {
-            return cache.addAll([
-                "/",
-                "/manifest.json",
-                "/logo.png",
-                "/assets/images/Anarta.png",
-                "/assets/images/Anarta2.png",
-                "/assets/images/Alesha.png",
-                "/assets/images/Alesha2.png",
-                "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free/css/all.min.css"
-            ]);
-        })
-    );
+const preLoad = function () {
+    return caches.open("offline").then(function (cache) {
+        // caching index and important routes
+        return cache.addAll(filesToCache);
+    });
+};
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(preLoad());
 });
 
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
-});
+const filesToCache = [
+    '/',
+    '/offline.html'
+];
 
-self.addEventListener("activate", (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames
-                    .filter((name) => name !== "koze-cache-v1")
-                    .map((name) => caches.delete(name))
-            );
-        })
-    );
+const checkResponse = function (request) {
+    return new Promise(function (fulfill, reject) {
+        fetch(request).then(function (response) {
+            if (response.status !== 404) {
+                fulfill(response);
+            } else {
+                reject();
+            }
+        }, reject);
+    });
+};
+
+const addToCache = function (request) {
+    // Only cache http(s) requests
+    if (!request.url.startsWith('http')) {
+        return Promise.resolve();
+    }
+    return caches.open("offline").then(function (cache) {
+        return fetch(request).then(function (response) {
+            return cache.put(request, response);
+        });
+    });
+};
+
+
+const returnFromCache = function (request) {
+    return caches.open("offline").then(function (cache) {
+        return cache.match(request).then(function (matching) {
+            if (!matching || matching.status === 404) {
+                return cache.match("offline.html");
+            } else {
+                return matching;
+            }
+        });
+    });
+};
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(checkResponse(event.request).catch(function () {
+        return returnFromCache(event.request);
+    }));
+    if(!event.request.url.startsWith('http')){
+        event.waitUntil(addToCache(event.request));
+    }
 });
